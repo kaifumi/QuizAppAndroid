9f63b598751e1d26c37e9b92e2d7105b6165f014
kaifumi <kaifumi1001@gmail.com>
2021-06-27 11:26:59 +0900

    #8 結果の表示とハイスコアの保存

--- app/src/main/java/com/kainolearn/quizapp/MainActivity.java
+++ app/src/main/java/com/kainolearn/quizapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -127,6 +128,9 @@ public class MainActivity extends AppCompatActivity {
                 // OKボタンが押された時の処理
                 if (quizCount == QUIZ_COUNT) {
                     // 結果画面を表示
+                    Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                    intent.putExtra("RIGHT_ANSWER_COUNT", rightAnswerCount);
+                    startActivity(intent);
                 } else {
                     quizCount++;
                     showNextQuiz();
--- app/src/main/java/com/kainolearn/quizapp/ResultActivity.java
+++ app/src/main/java/com/kainolearn/quizapp/ResultActivity.java
@@ -2,7 +2,9 @@ package com.kainolearn.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        TextView resultLabel = findViewById(R.id.resultLabel);
+        TextView totalScoreLabel = findViewById(R.id.totalScoreLabel);
+
+        // 正解数を取得
+        int score = getIntent().getIntExtra("RIGHT_ANSWER_COUNT", 0);
+
+        // トータルスコアを読み出し
+        SharedPreferences prefs = getSharedPreferences("quizApp", MODE_PRIVATE);
+        int totalScore = prefs.getInt("totalScore", 0);
+
+        // トータルスコアに今回のスコアを加算
+        totalScore += score;
+
+        // TextViewに表示する
+        resultLabel.setText(getString(R.string.result_score, score));
+        totalScoreLabel.setText(getString(R.string.result_total_score, totalScore));
+
+        // トータルスコアを保存
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("totalScore", totalScore);
+        editor.apply();
     }
 }
